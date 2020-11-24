c1703eeee6e85c87684f35bd6b128ec1a8b87422
Violaine Rouhet <rouhet.violaine@hotmail.fr>
2020-11-24 17:43:51 +0100

    start user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+
+    def show
+        @user = User.find(params[:id])    
+    end
+
+    private
+
+    def users_params
+        params.require(:user).permit(:first_name, :address, :email, :password)
+      end
+
 end
