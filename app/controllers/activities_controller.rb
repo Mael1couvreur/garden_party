77bb0c6ef46a3426fa64c70317f169ee2648e1a9
Mael1couvreur <mael.couvreur@kedgebs.com>
2020-11-29 19:19:59 +0100

    e

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,5 @@
 class ActivitiesController < ApplicationController
   def create
-    raise
     activity = Activity.new(activity_params)
     plant = Plant.find(params[:activity]["plant_id"])
     task = Task.find(params[:activity]["task_id"])
@@ -8,12 +7,13 @@ class ActivitiesController < ApplicationController
     activity.plant = plant
     activity.task = task
     activity.save!
+
     # redirect_to tasks_path
   end
 
   private
 
   def activity_params
-    params.require(:activity).permit(:plant, :task)
+    params.require(:activity).permit(:plant_id, :task_id)
   end
 end
