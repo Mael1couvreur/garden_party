8053e916a5f89401f9941f74752a307c928004c8
Mael1couvreur <mael.couvreur@kedgebs.com>
2020-12-01 16:54:40 +0100

    Text en fr

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -4,6 +4,8 @@ class Plant < ApplicationRecord
 
   has_many :activities
   has_many :tasks, through: :plant_information
-  
+
   has_one_attached :photo
+  validates :age_in_days, presence: true
+  validates :photo, presence: true
 end
